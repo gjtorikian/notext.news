a1efd08b97489f40f6b8638551b6f834ad45c653
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-11-28 12:18:12 +0000

    Start it up

---
+++ index.js
@@ -0,0 +1,74 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const request = require("request-promise-native");
+let cheerio = require("cheerio");
+
+const VIEWS_PATH = path.join(__dirname, "/views");
+
+let mustacheExpress = require("mustache-express");
+
+// Register '.mustache' extension with The Mustache Express
+app.engine("mustache", mustacheExpress());
+app.engine("mst", mustacheExpress(VIEWS_PATH + "/partials", ".mst"));
+
+app.set("view engine", "mustache");
+app.set("views", VIEWS_PATH);
+
+app.get("/", async function(req, res) {
+  res.setHeader("Content-Type", "text/html");
+
+  let options = {
+    uri: "http://www.nytimes.com",
+    transform: function(body) {
+      return cheerio.load(body);
+    }
+  };
+
+  let $ = await request(options);
+
+  localize($, "link", "href", "www.nytimes.com");
+  localize($, "script", "src", "www.nytimes.com");
+
+  // NYTimes ad space
+
+  $("div#app")
+    .children()
+    .first()
+    .remove();
+
+  textReplacer($, "*");
+
+  let contents = $.root().html();
+
+  res.send(contents);
+});
+
+function localize($, tag, attribute, url) {
+  $(tag).each(function(i, elem) {
+    let original = $(elem).attr(attribute);
+    // if (original !== undefined && original.length > 0 && original[0] == "/") {
+    // console.log(original);
+    $(elem).attr(attribute, `https://${url}${original}`);
+    // }
+  });
+}
+
+function textReplacer($, el) {
+  $(el)
+    .contents()
+    .each(function(i, elem) {
+      if (
+        elem.type == "text" &&
+        elem.parent.name != "style" &&
+        elem.parent.name != "script"
+      ) {
+        $(elem).replaceWith("&nbsp;");
+      }
+    });
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
